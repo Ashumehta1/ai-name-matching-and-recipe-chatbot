3d9e931f55bd84f43af7c5d1260da68054a983f7
Ashish Kumar <ashishrj08mehta@gmail.com>
2026-01-21 15:21:40 +0530

    list of name added

---
+++ ai_assignment/task1_name_matching/names_data.py
@@ -0,0 +1,9 @@
+names = [
+    "Geetha", "Gita", "Geeta", "Gitu", "Gitah",
+    "Sita", "Rita", "Meeta", "Neeta", "Seeta",
+    "Anita", "Sunita", "Kavita", "Savita",
+    "Reeta", "Preeti", "Priti", "Priya",
+    "Kiran", "Keerthi", "Kirti", "Keerti",
+    "Amit", "Sumit", "Rohit", "Mohit",
+    "Rahul", "Raul", "Rahil", "Rohil"
+]
